e5b54d3fd49d6d821cadcca550c883f63d234705
Justin Waltz <justinwaltz@Justins-iMac.local>
2019-07-18 15:07:23 -0500

    got titles of games to print on page

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 //console.log($);
 $(() => {
 //===============Search Div=============================
-let searchTerms = "";
+
 $("form").on("submit", (event) => {
     event.preventDefault();
-    searchTerms = $("input[type='text']").val();
-    bgaUrlInsert = bgaSearchUrl;
-    console.log(searchTerms);
-    console.log(bgaSearchUrl);
-    getBgaData();
+    const searchTerms = $("input[type='text']").val();
+    bgaUrlInsert = "https://www.boardgameatlas.com/api/search?name="+searchTerms+"&limit=10&client_id=tIPZB6stZR";
+    //console.log(searchTerms);
+    //console.log(bgaUrlInsert);
+    getBgaSearchResults();
   });
 
 //================API URL Variables=============================
 let bgaUrlInsert = "";
-let bgaSearchUrl = "search?name="+searchTerms+"&limit=10";
 
 //===============API Call Functions=============================
-const getBgaData = () => {
+const getBgaSearchResults = () => {
   $.ajax({
-        url:"https://www.boardgameatlas.com/api/"+bgaUrlInsert+"&client_id=tIPZB6stZR",
+        url: bgaUrlInsert,
       }).then(
         (data) => {
           console.log(data);
+          //console.log(data.games[0]);
+          for (var i = 0; i < 10; i++) {
+            const resultName = data.games[i].name;
+            console.log(resultName);
+            const $resultDisplay = $("<div>").text(resultName);
+            $(".search-section").append($resultDisplay);
+          }
+
           // $("#title").html(data.Title);
           // $("#year").html(data.Year);
           // $("#rating").html(data.Rated);
